bf1ce5c318e7a7a068c31cc8ea57ecca4c802e0f
Sumit Dhyani <66456021+Sumit2399@users.noreply.github.com>
2025-03-12 13:33:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.get("/products", async (req, res) => {
     }
 });
 
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000; // Azure will use its own port
+app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+
